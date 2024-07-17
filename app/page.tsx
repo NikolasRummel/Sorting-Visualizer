c774dcd6ed950dc21fe066021ea190749ced3321
Nikolas Rummel <nikolasrummel6@gmail.com>
2024-07-17 12:00:27 +0200

    added more to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,11 @@ import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} f
 import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
 import {Check, GithubIcon} from "lucide-react";
 import Link from "next/link";
-import { ProvidersContext } from "@/app/providers";
+import {ProvidersContext} from "@/app/providers";
+import AllAlgosPage from "@/app/all/page";
+import Visualizer from "@/components/vizualizer";
+import {mergeSort} from "@/lib/algorithms";
+import {generateRandomArray} from "@/lib/utils";
 
 const App: React.FC = () => {
     return (
@@ -18,7 +22,7 @@ const App: React.FC = () => {
 export default App;
 
 const Hero = () => {
-    const { setOpenSidebar } = React.useContext(ProvidersContext);
+    const {setOpenSidebar} = React.useContext(ProvidersContext);
 
     const handleOpenSidebar = () => {
         setOpenSidebar(true);
@@ -29,65 +33,71 @@ const Hero = () => {
     }, []);
 
     return (
-        <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
-            <div className="text-center lg:text-start space-y-6">
-                <main className="text-3xl font-bold">
-                    <h1 className="">
+        <div>
+            <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
+                <div className="text-center lg:text-start space-y-6">
+                    <main className="text-3xl font-bold">
+                        <h1 className="">
                         <span
                             className="inline bg-gradient-to-r from-[#3DB1FF]  to-[#1F73F9] text-transparent bg-clip-text text-8xl">
                             Algo Explorer
                         </span>{" "}
-                    </h1>{" "}
-                    learn{" "}
-                    <h2 className="inline">
+                        </h1>{" "}
+                        learn{" "}
+                        <h2 className="inline">
                         <span
-                            className="inline bg-gradient-to-r from-[#F596D3]  to-[#D247BF] text-transparent bg-clip-text">
+                            className="inline bg-gradient-to-r from-[#a61005]  to-[#ff4929] text-transparent bg-clip-text">
                             sorting and searching
                         </span>{" "}
-                        algorithms
-                    </h2>
-                </main>
+                            algorithms
+                        </h2>
+                    </main>
 
-                <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
-                    Visualize and understand sorting and search algorithms with interactive animations and clear
-                    explanations.
-                </p>
+                    <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
+                        Visualize and understand sorting and search algorithms with interactive animations and clear
+                        explanations.
+                    </p>
 
-                <div className="space-y-4 md:space-y-0 md:space-x-4">
-                    <Link href={"/quadratic/insertionsort"} passHref legacyBehavior>
-                        <Button className="w-full md:w-1/3" onClick={handleOpenSidebar}>Get Started</Button>
-                    </Link>
+                    <div className="space-y-4 md:space-y-0 md:space-x-4">
+                        <Link href={"/quadratic/insertionsort"} passHref legacyBehavior>
+                            <Button className="w-full md:w-1/3" onClick={handleOpenSidebar}>Get Started</Button>
+                        </Link>
 
-                    <a
-                        rel="noreferrer noopener"
-                        href="https://github.com/NikolasRummel/Sorting-Visualizer"
-                        target="_blank"
-                        className={`w-full md:w-1/3 ${buttonVariants({
-                            variant: "outline",
-                        })}`}
-                    >
-                        Github Repository
-                        <GithubIcon className="ml-2 w-5 h-5"/>
-                    </a>
+                        <a
+                            rel="noreferrer noopener"
+                            href="https://github.com/NikolasRummel/Sorting-Visualizer"
+                            target="_blank"
+                            className={`w-full md:w-1/3 ${buttonVariants({
+                                variant: "outline",
+                            })}`}
+                        >
+                            Github Repository
+                            <GithubIcon className="ml-2 w-5 h-5"/>
+                        </a>
+                    </div>
                 </div>
-            </div>
 
-            {/* Hero cards sections */}
-            <div className="z-10">
-                <HeroCards/>
-            </div>
+                {/* Hero cards sections */}
+                <div className="z-10">
+                    <HeroCards/>
+                </div>
 
-            {/* Shadow effect */}
-            <div className="shadow"></div>
-        </section>
-    );
+            </section>
+            <div className={"px-16"}>
+                <Visualizer algorithm={"MergeSort"} array={generateRandomArray(100)} isPaused={false}
+                            sortFunction={mergeSort} delay={100} soundMuted={true}/>
+            </div>
+        </div>
+    )
+        ;
 };
 
 const HeroCards = () => {
     return (
         <div className="hidden lg:flex flex-row flex-wrap gap-8 relative w-[700px] h-[500px]">
             {/* Testimonial */}
-            <Card className="absolute w-[340px] -top-[15px] left-[5px] drop-shadow-xl shadow-black/10 dark:shadow-white/10">
+            <Card
+                className="absolute w-[340px] -top-[15px] left-[5px] drop-shadow-xl shadow-black/10 dark:shadow-white/10">
                 <CardHeader className="flex flex-row items-center gap-4 pb-2">
                     <Avatar>
                         <AvatarImage
@@ -105,7 +115,6 @@ const HeroCards = () => {
 
                 <CardContent>This tool made understanding algorithms so much easier!</CardContent>
             </Card>
-
             {/* Team */}
             <Card
                 className="absolute right-[20px] top-4 w-80 flex flex-col justify-center items-center drop-shadow-xl shadow-black/10 dark:shadow-white/10">
@@ -123,7 +132,7 @@ const HeroCards = () => {
 
                 <CardContent className="text-center pb-2 text-sm">
                     <p>
-                       I hope you like my small project I have made for the datastructures and algorithms lecture.
+                        I hope you like my small project I have made for the datastructures and algorithms lecture.
                     </p>
                 </CardContent>
             </Card>
@@ -157,7 +166,8 @@ const HeroCards = () => {
                 </CardFooter>
             </Card>
 
-            <Card className="absolute w-[350px] right-[40px] bottom-[35px]  drop-shadow-xl shadow-black/10 dark:shadow-white/10">
+            <Card
+                className="absolute w-[350px] right-[40px] bottom-[35px]  drop-shadow-xl shadow-black/10 dark:shadow-white/10">
                 <CardHeader className="flex flex-row items-center gap-4 pb-2">
                     <Avatar>
                         <AvatarImage
